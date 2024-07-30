e63c4ebcbac54d292aad2438d7a115eed0064f93
Venzelland <101016773+Venzelland@users.noreply.github.com>
2024-07-30 16:45:24 +0300

    add initialRow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import './App.css'
+import {useState} from "react";
+
+type TableRowType = {
+    id: number
+    cells: boolean[]
+}
+
+const initialRows: TableRowType[] = [
+    {id: 0, cells: [false, true]},
+    {id: 1, cells: [true, false]}
+]
 
 function App() {
+    const [rows, setRows] = useState<TableRowType[]>(initialRows)
 
     return (
         <div>
@@ -10,26 +22,28 @@ function App() {
                 </colgroup>
                 <thead>
                 <tr>
-                    <th>№ п/п</th>
-                    <th>Наименование</th>
-                    <th>Цена, руб.</th>
+                    <th>#</th>
+                    <th>Обработка1</th>
+                    <th>Обработка2</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Карандаш цветной</td>
-                    <td>20,00</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Линейка 20 см</td>
-                    <td>30,00</td>
-                </tr>
+                {rows.map((row: TableRowType) => (
+                    <tr key={row.id}>
+                        <td>Заказ{row.id + 1}</td>
+                        {row.cells.map((cell, index) => (
+                                <td key={index}>{cell.toString()}</td>
+                            ))}
+                        <td>
+                            <button>редактировать</button>
+                            <button>удалить</button>
+                        </td>
+                    </tr>
+                ))}
                 </tbody>
-            </table>
-        </div>
-    )
-}
+                    </table>
+                    </div>
+                    )
+                }
 
 export default App
