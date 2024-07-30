5f6557be673849e07526235926d84cd3f9494a9d
Venzelland <101016773+Venzelland@users.noreply.github.com>
2024-07-30 16:20:40 +0300

    create table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <h1>Таблица</h1>
+            <table>
+                <colgroup>
+                </colgroup>
+                <thead>
+                <tr>
+                    <th>№ п/п</th>
+                    <th>Наименование</th>
+                    <th>Цена, руб.</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Карандаш цветной</td>
+                    <td>20,00</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Линейка 20 см</td>
+                    <td>30,00</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default App
